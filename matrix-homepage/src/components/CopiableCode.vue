aedd0c75a4e53426e1e750ad4e3dd8510becc598
wxx9248 <wxx9248@qq.com>
2023-01-28 17:48:59 -0500

    Upload Source Code

---
+++ matrix-homepage/src/components/CopiableCode.vue
@@ -0,0 +1,34 @@
+<template>
+    <VChip class="chip" @click="copyToClipboard(text)">
+        <code>{{text}}</code>
+    </VChip>
+</template>
+
+<script>
+export default {
+    name: "CopiableCode",
+    props: ["text", "disableCopy"],
+    methods: {
+        async copyToClipboard(text) {
+            try {
+                if (!this.disableCopy) {
+                    await navigator.clipboard.writeText(text);
+                    this.$emit("success");
+                }
+            } catch (error) {
+                this.$emit("error", error);
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+.chip {
+    cursor: pointer;
+    transition: 0.3s;
+}
+.chip:hover {
+    backdrop-filter: brightness(50%);
+}
+</style>
